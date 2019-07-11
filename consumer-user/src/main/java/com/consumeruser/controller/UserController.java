6405802afefdd6206b1a2922f236d4e2421d9cb1
850252329 <850252329@qq.com>
2019-07-11 22:05:11 +0800

    springcloud demo

---
+++ consumer-user/src/main/java/com/consumeruser/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.consumeruser.controller;
+
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.web.bind.annotation.GetMapping;
+        import org.springframework.web.bind.annotation.RestController;
+        import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class UserController {
+    @Autowired
+    RestTemplate restTemplate;
+
+    @GetMapping("/buy")
+    public String buyTicket(String name){
+        return name+"购买了"+restTemplate.getForObject("http://PROVIDER-TICKET/ticket",String.class);
+    }
+}
